cb76bbf69f21a8791c90edba3ce79c6745e4473a
foysal5965 <foysalahmed5965@gmail.com>
2022-10-20 15:09:00 +0600

    basic dragon news server

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000
+const cors = require('cors')
+app.use(cors())
+const categories = require('./data/category.json')
+const news = require('./data/news.json')
+
+app.get('/', (req, res) => {
+    res.send('appi is working')
+})
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+});
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if(id==='08'){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n=> n.category_id=== id);
+        res.send(categoryNews)
+    }
+})
+app.get('/news', (req, res)=>{
+    res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+app.listen(port, () => {
+    console.log('api is working on port', port);
+})
